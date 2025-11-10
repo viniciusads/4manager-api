68922a1b0bcd8c4121752c3e7646656bbf73a948
Bruno Gallotte <bruno.gallotte.ext@4techs.com.br>
2025-11-10 13:36:20 -0300

    feat: add customers commands

---
+++ src/4Manager.Application/Features/Customers/Commands/CreateCustomerCommand.cs
@@ -0,0 +1,15 @@
+using _4Tech._4Manager.Application.Features.Customers.Dtos;
+using MediatR;
+
+namespace _4Tech._4Manager.Application.Features.Customers.Commands
+{
+    public class CreateCustomerCommand : IRequest<CustomerResponseDto>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public CreateCustomerCommand(string name)
+        {
+            Name = name;
+        }
+    }
+}
