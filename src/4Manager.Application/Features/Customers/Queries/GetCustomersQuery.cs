424e34ff9d1ffbbf3b2b2e1c484aaacf48fce75a
Bruno Gallotte <bruno.gallotte.ext@4techs.com.br>
2025-12-03 11:43:21 -0300

    refactor: centralize pagination settings in Common/Pagination

--- src/4Manager.Application/Features/Customers/Queries/GetCustomersQuery.cs
+++ src/4Manager.Application/Features/Customers/Queries/GetCustomersQuery.cs
@@ -1,11 +1,12 @@
+using _4Tech._4Manager.Application.Common.Pagination;
 using _4Tech._4Manager.Application.Features.Customers.Dtos;
 using MediatR;
 
 namespace _4Tech._4Manager.Application.Features.Customers.Queries
 {
-    public record GetCustomersQuery : IRequest<IEnumerable<CustomerResponseDto>>
+    public record GetCustomersQuery : IRequest<IEnumerable<CustomerResponseDto>>, IPagedQuery
     {
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
+        public int PageNumber { get; set; } = PaginationDefaults.DefaultPageNumber;
+        public int PageSize { get; set; } = PaginationDefaults.DefaultPageSize;
     }
 }
